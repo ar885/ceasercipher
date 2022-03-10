12fbb5fe215430e7818e7369e8e3d09bc9214328
ar885 <arihantvardhan0865@gmail.com>
2022-03-10 20:34:34 +0530

    CEASER CIPHER PROGRAM IN PYTHON

---
+++ ceasercipher.py
@@ -0,0 +1,44 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+m= input("Enter the message to be Encrypted or decrypted :" );
+
+print("ENTER:(1 or 2) \n 1.Encryption \n2. Decryption" );
+
+n = int(input());
+result="";
+
+if(n==1):
+    k=int(input("Key value :"));
+    for i in range(len(m)):
+        char=m[i];
+        if(char.isupper()):
+            result+=chr((ord(char)+k-65)%26 +65);
+        else:
+            if (char.islower()):
+                result+=chr((ord(char)+k-97)%26 +97);
+            else:
+                result+=chr((ord(char)));
+    print("Encrypted Message:"+ result);
+if (n==2):
+    dk=int(input("Decryption Key Value:"));
+    for i in range(len(m)):
+        char=m[i];
+        if(char.isupper()):
+            result+=chr((ord(char)-dk-65)%26 +65);
+        else:
+            if (char.islower()):
+                result+=chr((ord(char)-dk-97)%26 +97);
+            else:
+                result+=chr((ord(char)));
+    print("Decrypted Message : "+result);
+if (n>2 or n<1):
+    print("[+] choose correct option [+]")
+            
+    
+
